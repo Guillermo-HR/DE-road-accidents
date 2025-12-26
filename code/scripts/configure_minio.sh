766b814fe63207de0f8bf9bcab289100630900f3
Guillermo Hernandez Ruiz de Esparza <guillermo.hre@gmail.com>
2025-12-26 14:18:33 -0600

    Add MinIO user credentials to environment configuration and update docker-compose

--- code/scripts/configure_minio.sh
+++ code/scripts/configure_minio.sh
@@ -8,11 +8,20 @@ until mc alias set $ALIAS http://$MINIO_HOST:$MINIO_PORT $MINIO_ROOT_USER $MINIO
     sleep 1
 done
 
+until mc admin user add $ALIAS $MINIO_RW_USER $MINIO_RW_PASSWORD; do
+    sleep 1
+done
+
+until mc admin user add $ALIAS $MINIO_R_USER $MINIO_R_PASSWORD; do
+    sleep 1
+done
+
+mc admin policy attach $ALIAS readwrite --user $MINIO_RW_USER
+mc admin policy attach $ALIAS readonly --user $MINIO_R_USER
+
 mc mb --ignore-existing $ALIAS/landing
 mc mb --ignore-existing $ALIAS/bronze
 mc mb --ignore-existing $ALIAS/silver
 
-echo "--- Buckets on the system: ---"
-mc ls $ALIAS
 
 tail -f /dev/null
